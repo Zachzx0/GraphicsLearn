6f271f052c46ae41c39a9f46cf69202e5878da30
Zachzx0 <zachzx0@gmail.com>
2018-07-29 17:14:51 +0800

    this is compelet reversion

--- GraphicLearn/Assets/Script/CreateObj.cs
+++ GraphicLearn/Assets/Script/CreateObj.cs
@@ -20,13 +20,13 @@ public class CreateObj : MonoBehaviour {
     List<Vector3> vertexPosList;
     List<Vector2> uvPosList;
     List<Vector3> vertexNormalPosList;
-    List<Face[]> FaceList;
+    List<Face> FaceList;
     // Use this for initialization
     void Start () {
         vertexPosList = new List<Vector3>();
         uvPosList = new List<Vector2>();
         vertexNormalPosList = new List<Vector3>();
-        FaceList = new List<Face[]>();
+        FaceList = new List<Face>();
 
         StreamReader sR = File.OpenText(Application.streamingAssetsPath + objFilePath);
         string nextLine;
@@ -36,6 +36,43 @@ public class CreateObj : MonoBehaviour {
             ParseString(nextLine);
         }
         sR.Close();
+        GameObject obj = new GameObject("cubeFromObj");
+        GameObject newGO = Instantiate(obj, Vector3.zero, Quaternion.identity);
+        newGO.AddComponent<MeshFilter>();
+        MeshRenderer mr = newGO.AddComponent<MeshRenderer>();
+        Material mat = Resources.Load<Material>("Material/cubeTexture");
+        mr.material = mat;
+        Mesh cubeMesh = newGO.GetComponent<MeshFilter>().mesh;
+        cubeMesh.Clear();
+        List<Vector3> vertexes = new List<Vector3>();
+        List<int> UVList = new List<int>();
+        List<int> TriList = new List<int>();
+        for(int i = 0; i < uvPosList.Count; i++)
+        {
+            Face currFace = FaceList.Find((face) =>
+            {
+                return face.uv == i + 1;
+                });
+
+            vertexes.Add(vertexPosList[currFace.vertex - 1]);
+        }
+        
+        for(int i= 0; i < FaceList.Count; i+=4)
+        {
+            TriList.Add(FaceList[i].uv-1);
+            TriList.Add(FaceList[i+1].uv - 1);
+            TriList.Add(FaceList[i+2].uv - 1);
+            TriList.Add(FaceList[i+2].uv - 1);
+            TriList.Add(FaceList[i+3].uv - 1);
+            TriList.Add(FaceList[i].uv - 1);
+        }
+
+        cubeMesh.vertices = vertexes.ToArray();
+        cubeMesh.triangles = TriList.ToArray();
+        cubeMesh.uv = uvPosList.ToArray();
+        //Vector2[] uvArray = new Vector2[UVList.Count];
+        
+
     }
 	
 	// Update is called once per frame
@@ -58,17 +95,15 @@ public class CreateObj : MonoBehaviour {
                 vertexNormalPosList.Add(new Vector3(Str2Float(strArray[1]), Str2Float(strArray[2]), Str2Float(strArray[3])));
                 break;
             case KEY_FACE:
-                Face[] faces = new Face[4];
-                for(int i = 1; i < strArray.Length - 1; i++)
+                for(int i = 1; i < strArray.Length; i++)
                 {
                     string[] intStrArray = strArray[i].Split('/');
                     Face face = new Face();
                     face.vertex = int.Parse(intStrArray[0]);
                     face.uv = int.Parse(intStrArray[1]);
                     face.normal = int.Parse(intStrArray[2]);
-                    faces[i] = face;
+                    FaceList.Add(face);
                 }
-                FaceList.Add(faces);
                 break;
         }
     }
